313351ad5f7970c264ff7d18901ca841e3a44f30
Saikat Das <saikat.rog@gmail.com>
2024-02-01 00:49:55 +0530

    ass3 q3

--- ass 3/three.c
+++ ass 3/three.c
@@ -1,1 +1,56 @@
 // Q3. Implement the Multiplication of two matrices using Strassen’s Divide and Conquer approach
+#include <stdio.h>
+
+void strassenMultiply(int A[2][2], int B[2][2], int C[2][2]) {
+    int M1, M2, M3, M4, M5, M6, M7;
+
+    M1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
+    M2 = (A[1][0] + A[1][1]) * B[0][0];
+    M3 = A[0][0] * (B[0][1] - B[1][1]);
+    M4 = A[1][1] * (B[1][0] - B[0][0]);
+    M5 = (A[0][0] + A[0][1]) * B[1][1];
+    M6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
+    M7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
+
+    C[0][0] = M1 + M4 - M5 + M7;
+    C[0][1] = M3 + M5;
+    C[1][0] = M2 + M4;
+    C[1][1] = M1 - M2 + M3 + M6;
+}
+
+void display(int matrix[2][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int A[2][2], B[2][2], result[2][2];
+
+    // input for A
+    printf("Enter elements for Matrix A (2x2):\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Enter element A[%d][%d]: ", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // input for B
+    printf("\nEnter elements for Matrix B (2x2):\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Enter element B[%d][%d]: ", i, j);
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    strassenMultiply(A, B, result);
+
+    printf("\nResult Matrix:\n");
+    display(result);
+    return 0;
+}
